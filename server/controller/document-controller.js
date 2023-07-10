23c9463ab15a3359367fe76d1d165fa225347112
Nimishpandit <101637810+Nimishpandit@users.noreply.github.com>
2023-07-10 17:45:10 +0530

    Update document-controller.js

--- server/controller/document-controller.js
+++ server/controller/document-controller.js
@@ -3,12 +3,12 @@ import Document from "../schema/documentSchema.js";
 export const getDocument = async (id) => {
   if (id === null) return;
 
-  const document = await Document.findById(id);
+  const document = await Document.findById(id); //finding the document using id if not founded then creating another one
 
   if (document) return document;
   return await Document.create({ _id: id, data: "" });
 };
 
 export const updateDocument = async (id, data) => {
-  return await Document.findByIdAndUpdate(id, { data });
+  return await Document.findByIdAndUpdate(id, { data }); //updating the data
 };
